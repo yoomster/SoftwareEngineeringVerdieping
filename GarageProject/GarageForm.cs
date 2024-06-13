6397e5557643bb93d0f05ed6b8ff666b44a83882
yoomster <nperenboom@hotmail.com>
2024-06-13 11:25:59 +0200

    added initialize garage and populate listbox

--- GarageProject/GarageForm.cs
+++ GarageProject/GarageForm.cs
@@ -12,9 +12,16 @@ namespace GarageProject
 {
     public partial class GarageForm : Form
     {
+        private GarageModel garage;
         public GarageForm()
         {
             InitializeComponent();
+        }
+
+        private void GarageForm_Load(object sender, EventArgs e)
+        {
+            InitializeGarage();
+            PopulateCarListBox();
         }
 
         private void buttonStartCar_Click(object sender, EventArgs e)
@@ -23,9 +30,40 @@ namespace GarageProject
             driveForm.ShowDialog();
         }
 
-        private void GarageForm_Load(object sender, EventArgs e)
+        private void SaveSelectedCar()
         {
+            garage.SelectCar(listBoxCars.SelectedItem);
+        }
 
+        private void InitializeGarage()
+        {
+            garage = new GarageModel
+            {
+                Name = "Naomis Garage"
+            };
+
+            CarModel toyota = new CarModel();
+            toyota.SetManufacturer("Toyota");
+            toyota.SetModel("Yaris");
+            toyota.SetMaxSpeed(170);
+
+            CarModel tesla = new CarModel();
+            tesla.SetManufacturer("Tesla");
+            tesla.SetModel("Cross");
+            toyota.SetMaxSpeed(260);
+
+            garage.AddCar(toyota);
+            garage.AddCar(tesla);
+        }
+
+        private void PopulateCarListBox()
+        {
+            listBoxCars.Items.Clear();
+
+            foreach (var car in garage.Cars)
+            {
+                listBoxCars.Items.Add($"{car.GetCarManufacturerAndModel()}");
+            }
         }
     }
 }
